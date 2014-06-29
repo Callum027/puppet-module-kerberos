1f64d81edbee7f60cd684226585854aa7c88dfa6
Callum027 <callum@huttradio.co.nz>
2014-06-29 21:41:17 +1200

    *headdesk*

--- manifests/server/kdc.pp
+++ manifests/server/kdc.pp
@@ -102,7 +102,7 @@ class kerberos::server::kdc(
   # Look up our users in hiera.  Create a principal for each one listed
   # for this realm.
   $kerberos_principals = hiera("kerberos_principals", [])
-  if (!is_hash($kerberos_principals)) {
+  if (is_hash($kerberos_principals)) {
     create_resources('kerberos::addprinc',$kerberos_principals)
   }
 
